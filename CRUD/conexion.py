068bc018f42de178aa28c06a036052f5a80eb75d
Agustin Gordillo <145617850+AtomGz1@users.noreply.github.com>
2024-04-04 04:22:08 -0300

    Update conexion.py

--- CRUD/conexion.py
+++ CRUD/conexion.py
@@ -3,8 +3,8 @@ import mysql.connector
 class BaseDatos:
     def __init__(self):
         self.connection = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
+            host = '',
+            user = '',
             password = '',
             database = ''
         )
